84ca5fed1fb337a151e9d382482de8f8e0a8efd4
shailendra-kindlebit <shailendra.yadav@kindlebit.org>
2026-05-14 15:03:56 +0530

    Initial Commit

---
+++ VigilantTests/VigilantTests.swift
@@ -0,0 +1,19 @@
+//
+//  VigilantTests.swift
+//  VigilantTests
+//
+//  Created by KBS on 5/14/26.
+//
+
+import Testing
+@testable import Vigilant
+
+struct VigilantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
